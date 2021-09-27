a449558513f672244d5f5e8087544df78916ccc2
ojaswi866 <78652112+ojaswi866@users.noreply.github.com>
2021-09-26 21:05:27 -0400

    Add files via upload

---
+++ js/System.js
@@ -0,0 +1,14 @@
+class System{
+
+    constructor(){}
+
+    authenticate(actualCode,enteredCode){
+    if(actualCode === enteredCode)
+    return true
+    else
+     return false
+    }
+    
+
+
+}
